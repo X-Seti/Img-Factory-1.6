ff1eee4c3ab7292150194dc51d5636ed1a18bd54
Claudia <xseti@github.com>
2026-03-05 18:49:10 +0000

    Revert: corner button back to d2571d7 state (fixedSize 28, icon 20, margin-top 3px)

--- apps/components/Img_Factory/imgfactory.py
+++ apps/components/Img_Factory/imgfactory.py
@@ -2769,10 +2769,12 @@ class IMGFactory(QMainWindow):
                     splitter.setSizes(s)
 
             _tog_btn = QPushButton()
-            _tog_btn.setIcon(get_panel_toggle_icon())
-            _tog_btn.setMinimumSize(32, 32)
-            _tog_btn.setMaximumSize(32, 32)
+            _tog_btn.setIcon(get_panel_toggle_icon(20))
+            _tog_btn.setFixedSize(28, 28)
+            _tog_btn.setIconSize(QSize(20, 20))
             _tog_btn.setToolTip("Toggle: tabs full / split")
+            _tog_btn.setContentsMargins(0, 0, 0, 0)
+            _tog_btn.setStyleSheet("QPushButton { margin-top: 3px; margin-right: 2px; padding: 2px; border: none; }")
             _tog_btn.clicked.connect(_on_toggle)
             self.main_tab_widget.setCornerWidget(_tog_btn, Qt.Corner.TopRightCorner)
             self._panel_toggle_btn = _tog_btn
